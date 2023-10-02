80ee69b1bd9e4c1baff40488eba42e57886fcfe5
patuspatinus <100175411+patuspatinus@users.noreply.github.com>
2023-10-02 19:18:29 +0700

    Process data

--- Process_data/combine_json.py
+++ Process_data/combine_json.py
@@ -1,8 +1,8 @@
 import json
 
 # Paths to the input JSON files
-file1_path = '/workspace/ailab/phucnd/25092023_109_Nhom_chung/json_label_TTKV_new.json'
-file2_path = '/workspace/ailab/phucnd/25092023_112_Nhom_benh/json_label_TTKV_new.json'
+file1_path = '/workspace/ailab/phucnd/25092023_109_Nhom_chung/json_label_TTKV_final.json'
+file2_path = '/workspace/ailab/phucnd/25092023_112_Nhom_benh/json_label_TTKV_final.json'
 
 # Path to the output JSON file
 output_path = '/workspace/ailab/phucnd/labels_TTKV_new.json'
