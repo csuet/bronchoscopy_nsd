e833975e4c4d5d2a126289b3f96768bff9aa5dfa
patuspatinus <100175411+patuspatinus@users.noreply.github.com>
2023-10-04 16:22:55 +0700

    Add files via upload

---
+++ ESFPNet_base/infer_segment.py
@@ -0,0 +1,254 @@
+# loading in and transforming data
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset,DataLoader,ConcatDataset
+from torch.autograd import Variable
+
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+#from skimage import io, transform
+from PIL import Image
+
+# visualizing data
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+
+# load dataset information
+import yaml
+
+import json
+# image writing
+import imageio
+from skimage import img_as_ubyte
+
+# Clear GPU cache
+torch.cuda.empty_cache()
+
+model_type = 'B4'
+
+init_trainsize = 352
+
+class test_dataset:
+    def __init__(self, image_root, gt_root, testsize):
+        self.testsize = testsize
+        self.images = [image_root + f for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
+        self.gts = [gt_root + f for f in os.listdir(gt_root) if f.endswith('.tif') or f.endswith('.png') or f.endswith('.jpg')]
+        self.images = sorted(self.images)
+        self.gts = sorted(self.gts)
+        self.transform = transforms.Compose([
+            transforms.Resize((self.testsize, self.testsize)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])])
+        self.gt_transform = transforms.ToTensor()
+        self.size = len(self.images)
+        self.index = 0
+
+    def load_data(self):
+        image = self.rgb_loader(self.images[self.index])
+        image = self.transform(image).unsqueeze(0)
+        gt = self.binary_loader(self.gts[self.index])
+        name = self.images[self.index].split('/')[-1]
+        if name.endswith('.jpg'):
+            name = name.split('.jpg')[0] + '.png'
+        self.index += 1
+        return image, gt, name
+
+    def rgb_loader(self, path):
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('RGB')
+
+    def binary_loader(self, path):
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('L')
+        
+from collections import OrderedDict
+import copy
+
+from Encoder import mit
+from Decoder import mlp
+from mmcv.cnn import ConvModule
+
+class ESFPNetStructure(nn.Module):
+
+    def __init__(self, embedding_dim = 160):
+        super(ESFPNetStructure, self).__init__()
+
+        # Backbone
+        if model_type == 'B0':
+            self.backbone = mit.mit_b0()
+        if model_type == 'B1':
+            self.backbone = mit.mit_b1()
+        if model_type == 'B2':
+            self.backbone = mit.mit_b2()
+        if model_type == 'B3':
+            self.backbone = mit.mit_b3()
+        if model_type == 'B4':
+            self.backbone = mit.mit_b4()
+        if model_type == 'B5':
+            self.backbone = mit.mit_b5()
+
+        self._init_weights()  # load pretrain
+
+        # LP Header
+        self.LP_1 = mlp.LP(input_dim = self.backbone.embed_dims[0], embed_dim = self.backbone.embed_dims[0])
+        self.LP_2 = mlp.LP(input_dim = self.backbone.embed_dims[1], embed_dim = self.backbone.embed_dims[1])
+        self.LP_3 = mlp.LP(input_dim = self.backbone.embed_dims[2], embed_dim = self.backbone.embed_dims[2])
+        self.LP_4 = mlp.LP(input_dim = self.backbone.embed_dims[3], embed_dim = self.backbone.embed_dims[3])
+
+        # Linear Fuse
+        self.linear_fuse34 = ConvModule(in_channels=(self.backbone.embed_dims[2] + self.backbone.embed_dims[3]), out_channels=self.backbone.embed_dims[2], kernel_size=1,norm_cfg=dict(type='BN', requires_grad=True))
+        self.linear_fuse23 = ConvModule(in_channels=(self.backbone.embed_dims[1] + self.backbone.embed_dims[2]), out_channels=self.backbone.embed_dims[1], kernel_size=1,norm_cfg=dict(type='BN', requires_grad=True))
+        self.linear_fuse12 = ConvModule(in_channels=(self.backbone.embed_dims[0] + self.backbone.embed_dims[1]), out_channels=self.backbone.embed_dims[0], kernel_size=1,norm_cfg=dict(type='BN', requires_grad=True))
+
+        # Fused LP Header
+        self.LP_12 = mlp.LP(input_dim = self.backbone.embed_dims[0], embed_dim = self.backbone.embed_dims[0])
+        self.LP_23 = mlp.LP(input_dim = self.backbone.embed_dims[1], embed_dim = self.backbone.embed_dims[1])
+        self.LP_34 = mlp.LP(input_dim = self.backbone.embed_dims[2], embed_dim = self.backbone.embed_dims[2])
+
+        # Final Linear Prediction
+        self.linear_pred = nn.Conv2d((self.backbone.embed_dims[0] + self.backbone.embed_dims[1] + self.backbone.embed_dims[2] + self.backbone.embed_dims[3]), 1, kernel_size=1)
+
+    def _init_weights(self):
+
+        if model_type == 'B0':
+            pretrained_dict = torch.load('./Pretrained/mit_b0.pth')
+        if model_type == 'B1':
+            pretrained_dict = torch.load('./Pretrained/mit_b1.pth')
+        if model_type == 'B2':
+            pretrained_dict = torch.load('./Pretrained/mit_b2.pth')
+        if model_type == 'B3':
+            pretrained_dict = torch.load('./Pretrained/mit_b3.pth')
+        if model_type == 'B4':
+            pretrained_dict = torch.load('./Pretrained/mit_b4.pth')
+        if model_type == 'B5':
+            pretrained_dict = torch.load('./Pretrained/mit_b5.pth')
+
+
+        model_dict = self.backbone.state_dict()
+        pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+        model_dict.update(pretrained_dict)
+        self.backbone.load_state_dict(model_dict)
+        print("successfully loaded!!!!")
+
+
+    def forward(self, x):
+
+        ##################  Go through backbone ###################
+
+        B = x.shape[0]
+
+        #stage 1
+        out_1, H, W = self.backbone.patch_embed1(x)
+        for i, blk in enumerate(self.backbone.block1):
+            out_1 = blk(out_1, H, W)
+        out_1 = self.backbone.norm1(out_1)
+        out_1 = out_1.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()  #(Batch_Size, self.backbone.embed_dims[0], 88, 88)
+
+        # stage 2
+        out_2, H, W = self.backbone.patch_embed2(out_1)
+        for i, blk in enumerate(self.backbone.block2):
+            out_2 = blk(out_2, H, W)
+        out_2 = self.backbone.norm2(out_2)
+        out_2 = out_2.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()  #(Batch_Size, self.backbone.embed_dims[1], 44, 44)
+
+        # stage 3
+        out_3, H, W = self.backbone.patch_embed3(out_2)
+        for i, blk in enumerate(self.backbone.block3):
+            out_3 = blk(out_3, H, W)
+        out_3 = self.backbone.norm3(out_3)
+        out_3 = out_3.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()  #(Batch_Size, self.backbone.embed_dims[2], 22, 22)
+
+        # stage 4
+        out_4, H, W = self.backbone.patch_embed4(out_3)
+        for i, blk in enumerate(self.backbone.block4):
+            out_4 = blk(out_4, H, W)
+        out_4 = self.backbone.norm4(out_4)
+        out_4 = out_4.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()  #(Batch_Size, self.backbone.embed_dims[3], 11, 11)
+
+
+        #segmentation
+        # go through LP Header
+        lp_1 = self.LP_1(out_1)
+        lp_2 = self.LP_2(out_2)
+        lp_3 = self.LP_3(out_3)
+        lp_4 = self.LP_4(out_4)
+
+        # linear fuse and go pass LP Header
+        lp_34 = self.LP_34(self.linear_fuse34(torch.cat([lp_3, F.interpolate(lp_4,scale_factor=2,mode='bilinear', align_corners=False)], dim=1)))
+        lp_23 = self.LP_23(self.linear_fuse23(torch.cat([lp_2, F.interpolate(lp_34,scale_factor=2,mode='bilinear', align_corners=False)], dim=1)))
+        lp_12 = self.LP_12(self.linear_fuse12(torch.cat([lp_1, F.interpolate(lp_23,scale_factor=2,mode='bilinear', align_corners=False)], dim=1)))
+
+        # get the final output
+        lp4_resized = F.interpolate(lp_4,scale_factor=8,mode='bilinear', align_corners=False)
+        lp3_resized = F.interpolate(lp_34,scale_factor=4,mode='bilinear', align_corners=False)
+        lp2_resized = F.interpolate(lp_23,scale_factor=2,mode='bilinear', align_corners=False)
+        lp1_resized = lp_12
+
+        out1 = self.linear_pred(torch.cat([lp1_resized, lp2_resized, lp3_resized, lp4_resized], dim=1))
+        # print(out.shape)
+        return out1
+test_images_path = './dataset/Lung_cancer_lesions/test/imgs'
+test_masks_path = './dataset/Lung_cancer_lesions/test/masks'
+
+def saveResult():
+
+    data = 'Lung_cancer_lesions'
+    save_path = './log_dir/' + data + '/'
+    os.makedirs(save_path, exist_ok=True)
+
+    ESFPNet = torch.load('./SaveModel/Lung_cancer_lesions/Segmentation_model.pt')
+    ESFPNet.eval()
+
+    val = 0
+    count = 0
+
+    smooth = 1e-4
+    val_loader = test_dataset(test_images_path + '/',test_masks_path + '/' ,init_trainsize) #
+    for i in range(val_loader.size):
+        image, gt, name = val_loader.load_data()#
+        gt = np.asarray(gt, np.float32)
+        gt /= (gt.max() + 1e-8)
+
+        image = image.cuda()
+
+        pred1= ESFPNet(image)
+        pred1 = F.upsample(pred1, size=gt.shape, mode='bilinear', align_corners=False)
+        pred1 = pred1.sigmoid()
+        threshold = torch.tensor([0.5]).to(device)
+        pred1 = (pred1 > threshold).float() * 1
+
+        pred1 = pred1.data.cpu().numpy().squeeze()
+        pred1 = (pred1 - pred1.min()) / (pred1.max() - pred1.min() + 1e-8)
+        target = np.array(gt)
+
+        input_flat = np.reshape(pred1,(-1))
+        target_flat = np.reshape(target,(-1))
+        intersection = (input_flat*target_flat)
+        loss =  (2 * intersection.sum() + smooth) / (pred1.sum() + target.sum() + smooth)
+
+        a =  '{:.4f}'.format(loss)
+        a = float(a)
+
+        val = val + a
+        count = count + 1
+
+    print("dice_val_segmetation",100 * val/count)
+
+
+        
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print('Models moved to GPU.')
+else:
+    print('Only CPU available.')
+
+saveResult()
