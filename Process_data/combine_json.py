af2294bb200047e25894049f552ab4522d93a93c
patuspatinus <100175411+patuspatinus@users.noreply.github.com>
2023-10-06 00:36:57 +0700

    Add files via upload

--- Process_data/combine_json.py
+++ Process_data/combine_json.py
@@ -1,11 +1,26 @@
-import json
 # Combine two json files
+import json
+import argparse
+from pathlib import Path
+import os
+
+parser = argparse.ArgumentParser("Preprocess data")
+parser.add_argument('--data_json_labels_cancer', type=str, required=True,
+        help='Location of the data directory containing json_label files of cancer cases')
+parser.add_argument('--data_json_labels_non_cancer', type=str, required=True,
+        help='Location of the data directory containing json_label files of non cancer cases')
+parser.add_argument('--path_save', type=str, required=True,
+        help='Location of the saved file)')
+args = parser.parse_args()
+
 # Paths to the input JSON files
-file1_path = '/workspace/ailab/phucnd/04102023_Nhom_benh/json_label_TTKV_final.json'
-file2_path = '/workspace/ailab/phucnd/04102023_Nhom_chung/json_label_TTKV_final.json'
+file1_path = Path(args.data_json_labels_cancer)
+file2_path = Path(args.data_json_labels_non_cancer)
 
 # Path to the output JSON file
-output_path = '/workspace/ailab/phucnd/labels_TTKV.json'
+output_save_path = Path(args.path_save)
+if not os.path.exists(output_save_path):
+    os.makedirs(output_save_path)
 
 with open(file1_path, 'r') as file1:
     data1 = json.load(file1)
@@ -17,5 +32,5 @@ with open(file2_path, 'r') as file2:
 combined_data = data1 + data2
 
 # Write the combined data to a new JSON file
-with open(output_path, 'w') as output_file:
+with open(output_save_path, 'w') as output_file:
     json.dump(combined_data, output_file, indent=4)
