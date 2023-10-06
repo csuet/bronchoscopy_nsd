c59a28f5f38a0e016b358fa3d1c9bc5df4df3d74
patuspatinus <100175411+patuspatinus@users.noreply.github.com>
2023-10-06 16:01:46 +0700

    Add files via upload

--- Process_data/annots_to_mask.py
+++ Process_data/annots_to_mask.py
@@ -38,18 +38,11 @@ data_labels = json.load(labels)
 shape_x = []
 shape_y = []
 
-# Labels of Anatomical landmark
-label_Anatomical_landmark = ["b2cc70ea-01f6-4389-a3a3-6222600a445a", "ce08e883-ddd2-4639-aa05-9fdac022f545",
-               "c93010c7-b77b-44a8-8866-7511f989e97a", "7e3b10f1-c8d2-4bf3-8250-1dc5954f2de5",
-               "a9d13324-3c88-4135-88bf-7fbb4ccb4e13", "1578d8da-1512-479c-aed7-ef7cd4fb5541",
-               "c115009a-d19a-4337-9151-0dd20a2562e7", "e572ea16-52e8-4404-b866-363eb3f733ce",
-               "27cc98cd-7cad-4134-bfb5-b2c74af3326e", "ccca7aa2-1593-4e16-a436-38a5516ce433",
-               "0ac584fb-aaeb-44f1-9fa1-fafd973ddac8"]
-# Labels of Lesions
-label_Lesions = ["a2ff1ceb-280b-410d-9ddc-82cb4a2e2ccb", "ff9241a0-a760-49d4-b34d-54f20ddedb0c",
-              "b96f104d-948d-4507-8b9c-c1dd0734b759", "4001669a-6f32-4a80-91f1-7c0766f19d29",
-              "cbed49c9-81ec-45d0-b619-a12c57c1770f", "3f2cce73-e832-4e8c-93f3-984b9e48baf3",
-              "cf97e212-5790-4ce0-92b0-5f585cfbbd8c"]
+# Labels of Anatomical landmark = ["Vocal cords", "Main carina","Intermediate bronchus","Right superior lobar bronchus","Right inferior lobar bronchus","Right middle lobar bronchus","Left inferior lobar bronchus","Left superior lobar bronchus","Right main bronchus","Left main bronchus","Trachea"]
+label_Anatomical_landmark = ["b2cc70ea-01f6-4389-a3a3-6222600a445a", "ce08e883-ddd2-4639-aa05-9fdac022f545","c93010c7-b77b-44a8-8866-7511f989e97a", "7e3b10f1-c8d2-4bf3-8250-1dc5954f2de5","a9d13324-3c88-4135-88bf-7fbb4ccb4e13", "1578d8da-1512-479c-aed7-ef7cd4fb5541","c115009a-d19a-4337-9151-0dd20a2562e7", "e572ea16-52e8-4404-b866-363eb3f733ce","27cc98cd-7cad-4134-bfb5-b2c74af3326e", "ccca7aa2-1593-4e16-a436-38a5516ce433","0ac584fb-aaeb-44f1-9fa1-fafd973ddac8"]
+# Labels of Lesions = ["Mucosal infiltration", "Mucosal edema of carina","Vascular growth","Muscosal erythema","Tumor","Anthrocosis","Stenosis"]
+label_Lesions = ["a2ff1ceb-280b-410d-9ddc-82cb4a2e2ccb", "ff9241a0-a760-49d4-b34d-54f20ddedb0c","b96f104d-948d-4507-8b9c-c1dd0734b759", "4001669a-6f32-4a80-91f1-7c0766f19d29","cbed49c9-81ec-45d0-b619-a12c57c1770f", "3f2cce73-e832-4e8c-93f3-984b9e48baf3","cf97e212-5790-4ce0-92b0-5f585cfbbd8c"]
+
 
 # Create ground truth for segmentation tasks
 for i in data_objects:
