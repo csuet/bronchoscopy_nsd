fe8b1d923c95be22dd4adaac9477d9e3abc1dd13
patuspatinus <100175411+patuspatinus@users.noreply.github.com>
2024-02-02 15:39:37 +0700

    Update

--- ESFPNet_base/train_segment_ESFPNet.py
+++ ESFPNet_base/train_segment_ESFPNet.py
@@ -37,14 +37,8 @@ from pathlib import Path
 parser = argparse.ArgumentParser("ESFPNet based model")
 parser.add_argument('--label_json_path', type=str, required=True,
         help='Location of the data directory containing json labels file of each task after combining two json files.json')
-parser.add_argument('--path_cancer_imgs', type=str, required=True,
-        help='Location of the images of cancer cases)')
-parser.add_argument('--path_non_cancer_imgs', type=str, required=True,
-        help='Location of the images of non cancer cases)')
-parser.add_argument('--path_cancer_masks', type=str, required=True,
-        help='Location of the masks of cancer cases for each tasks)')
-parser.add_argument('--path_non_cancer_masks', type=str, required=True,
-        help='Location of the masks of non cancer cases for each tasks)')
+parser.add_argument('--dataset', type=str, required=True,
+        help='Location of the dataset for each tasks')
 parser.add_argument('--model_type', type=str, default='B4',
         help='Type of model (default B4)')
 parser.add_argument('--init_trainsize', type=int, default=352,
@@ -53,10 +47,8 @@ parser.add_argument('--batch_size', type=int, default=8,
         help='Batch size for training (default = 8)')
 parser.add_argument('--n_epochs', type=int, default=500,
         help='Number of epochs for training (default = 500)')
-parser.add_argument('--if_renew', type=bool, default=True,
-        help='Check if split data to train_val_test')
-parser.add_argument('--task', type=str, default='Anatomical_Landmarks',
-        help='Task: Anatomical_Landmarks or Lung_cancer_lesions')
+parser.add_argument('--task', type=str, required=True,
+        help='Task: Anatomical_landmarks or Lung_lesions')
 args = parser.parse_args()
 
 
@@ -68,164 +60,12 @@ import wandb
 wandb.login()
 wandb.init(project="ESFPNet_segmetation_task_on_" + args.task)
 
-class SplittingDataset(Dataset):
-    """
-    dataloader for polyp segmentation tasks
-    """
-    def __init__(self, image_root, gt_root):
-
-        self.images = []
-        
-        for root, dirs, files in os.walk(image_root):
-            for file in files:
-                if file.endswith('.jpg') or file.endswith('.png'):
-                    self.images.append(os.path.join(root, file))
-        
-        self.gts = []
-
-        for root, dirs, files in os.walk(gt_root):
-            for file in files:
-                if file.endswith('.jpg') or file.endswith('.png'):
-                    self.gts.append(os.path.join(root, file))
-                    
-        # or /masks_Anatomical_Landmarks
-        mask_dir = "/masks_" + args.task + "/"
-        self.images = [file for file in self.images if file.replace('/imgs/', mask_dir) in self.gts]
-        
-        self.images = sorted(self.images)
-        self.gts = sorted(self.gts)
-        self.filter_files()
-        self.size = len(self.images)
-        self.transform = transforms.ToTensor()
-
-    def __getitem__(self, index):
-        
-        image = self.rgb_loader(self.images[index])
-        gt = self.binary_loader(self.gts[index])
-        
-        name = self.images[index].split('/')[-1]
-        return self.transform(image), self.transform(gt), name
-
-    def filter_files(self):
-        assert len(self.images) == len(self.gts)
-        images = []
-        gts = []
-        for img_path, gt_path in zip(self.images, self.gts):
-            img = Image.open(img_path)
-            gt = Image.open(gt_path)
-            if img.size == gt.size:
-                images.append(img_path)
-                gts.append(gt_path)
-        self.images = images
-        self.gts = gts
-
-    def rgb_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            return img.convert('RGB')
-
-    def binary_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            return img.convert('L')
-
-    def __len__(self):
-        return self.size
-
-
-def splitDataset(renew):
-    split_train_images_save_path = './dataset/' + args.task+ '/train/imgs'
-    os.makedirs(split_train_images_save_path, exist_ok=True)
-    split_train_masks_save_path = './dataset/' + args.task + '/train/masks'
-    os.makedirs(split_train_masks_save_path, exist_ok=True)
-    
-    split_validation_images_save_path = './dataset/' + args.task + '/val/imgs'
-    os.makedirs(split_validation_images_save_path, exist_ok=True)
-    split_validation_masks_save_path ='./dataset/' + args.task + '/val/masks'
-    os.makedirs(split_validation_masks_save_path, exist_ok=True)
-    
-    split_test_images_save_path = './dataset/' + args.task + '/test/imgs'
-    os.makedirs(split_test_images_save_path, exist_ok=True)
-    split_test_masks_save_path = './dataset/' + args.task + '/test/masks'
-    os.makedirs(split_test_masks_save_path, exist_ok=True)
-    
-    if renew == True:
-        DatasetList = []
-
-        images_train_path_1 = Path(args.path_cancer_imgs)
-        masks_train_path_1 = Path(args.path_cancer_masks)
-        Dataset_part_train_1 = SplittingDataset(images_train_path_1, masks_train_path_1)
-        DatasetList.append(Dataset_part_train_1)
-
-        images_train_path_2 = Path(args.path_non_cancer_imgs)
-        masks_train_path_2 = Path(args.path_non_cancer_masks)
-        Dataset_part_train_2 = SplittingDataset(images_train_path_2, masks_train_path_2)
-        DatasetList.append(Dataset_part_train_2)
-
-        wholeDataset = ConcatDataset([DatasetList[0], DatasetList[1]])
-
-        imgs_list = []
-        masks_list = []
-        labels_list = []
-        names_list = []
-
-        for iter in list(wholeDataset):
-            imgs_list.append(iter[0])
-            masks_list.append(iter[1])
-            labels_list.append(iter[2])
-            names_list.append(iter[3])
-
-        element_counts = {}
-
-        for element in labels_list:
-            if element in element_counts:
-                element_counts[element] += 1
-            else:
-                element_counts[element] = 1
-
-        removed_elements = []
-
-        Y_data = [element for element in labels_list if element_counts[element] > 4]
-
-        for element in labels_list:
-            if element_counts[element] < 5:
-                removed_elements.append(element)
-
-        combine_list = list(zip(imgs_list, masks_list, names_list, labels_list))
-       
-        X_data = [tup for tup in combine_list if not any(item in removed_elements for item in tup)]
-
-        
-        X_train, X_test, y_train, y_test = train_test_split(X_data, Y_data, test_size=0.1, 
-                                                            random_state=42, stratify = Y_data) 
-
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, 
-                                                            random_state=42, stratify = y_train) 
-        
-        for i in X_train:
-            image, gt, name, str_label = i[0], i[1], i[2], i[3]
-            image_data = image.data.cpu().numpy().squeeze().transpose(1,2,0)
-            gt_data = gt.data.cpu().numpy().squeeze()
-            imageio.imwrite(split_train_images_save_path + '/' + name,img_as_ubyte(image_data))
-            imageio.imwrite(split_train_masks_save_path + '/' + name, img_as_ubyte(gt_data))
-        for i in X_val:
-            image, gt, name, str_label = i[0], i[1], i[2], i[3]
-            image_data = image.data.cpu().numpy().squeeze().transpose(1,2,0)
-            gt_data = gt.data.cpu().numpy().squeeze()
-            imageio.imwrite(split_validation_images_save_path + '/' + name,img_as_ubyte(image_data))
-            imageio.imwrite(split_validation_masks_save_path + '/' + name, img_as_ubyte(gt_data))
-        
-        for i in X_test:
-            image, gt, name, str_label = i[0], i[1], i[2], i[3]
-            image_data = image.data.cpu().numpy().squeeze().transpose(1,2,0)
-            gt_data = gt.data.cpu().numpy().squeeze()
-            imageio.imwrite(split_test_images_save_path + '/' + name,img_as_ubyte(image_data))
-            imageio.imwrite(split_test_masks_save_path + '/' + name, img_as_ubyte(gt_data))
-
-    return split_train_images_save_path, split_train_masks_save_path, split_validation_images_save_path, split_validation_masks_save_path, split_test_images_save_path, split_test_masks_save_path
-
-train_images_path, train_masks_path, val_images_path, val_masks_path, test_images_path, test_masks_path = splitDataset(args.if_renew)
-
+train_images_path = args.dataset + '/train/imgs'
+train_masks_path  = args.dataset + '/train/masks'
+val_images_path   = args.dataset + '/val/imgs'
+val_masks_path    = args.dataset + '/val/masks'
+test_images_path  = args.dataset + '/test/imgs' 
+test_masks_path   = args.dataset + '/test/masks'
 
 class PolypDataset(Dataset):
     """
@@ -626,7 +466,7 @@ def training_loop(n_epochs, ESFPNet_optimizer, numIters):
             wandb.log({"dice_val" : validation_coeff})
             if coeff_max < validation_coeff:
                 coeff_max = validation_coeff
-                save_model_path = './SaveModel/' + data +'/'
+                save_model_path = './SaveModel/' + args.task +'/'
                 os.makedirs(save_model_path, exist_ok=True)
                 print(save_model_path)
                 torch.save(ESFPNet, save_model_path + '/Segmentation_model.pt')
